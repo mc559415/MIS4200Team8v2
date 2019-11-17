a886800862ae0c9d501a2aa6dd825f6af954c96c
mm548215 <mm548215@COB-CLAB59>
2019-11-17 17:30:34 -0500

    push1

--- Models/sendPoints.cs
+++ Models/sendPoints.cs
@@ -8,27 +8,16 @@ using System.Data.Entity;
 using System.ComponentModel.DataAnnotations.Schema;
 
 
+
+
 namespace MIS4200Team8v2.Models
 {
     public class sendPoints
     {
-        [Key]
-        [Required]
-        public int userID { get; set; }
-        [Required]
-        [Display(Name = "First Name")]
-        public string firstName { get; set; }
-        [Required]
-        [Display(Name = "Last Name")]
-        public string lastName { get; set; }
-        [Required]
-        [Display(Name = "Core Value")]
-        public string coreValue { get; set; }
-        [Required]
-        [Display(Name = "Points")]
         public int points { get; set; }
-        public Guid userID { get; set; }
-        [ForeignKey("userID")]
-        public virtual UserDetail UserDetail { get; set; }
+        public int userID { get; set; }
+        
+
+
     }
 }
---
+++ Models/userDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MIS4200Team8v2.Models
+{
+    public class userDetail
+    {
+        public int userID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string positionTitle { get; set; }
+        ICollection<sendPoints> sendPoints { get; set; }
+
+    }
+}
