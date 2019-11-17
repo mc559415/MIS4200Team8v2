abd9b8d8f7decf43c890e92003925894830f6a9c
mc559415 <mc559415@ohio.edu>
2019-11-17 16:39:05 -0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MIS4200Team8v2.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MIS4200Team8v2.Startup))]
+namespace MIS4200Team8v2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
