47c16562cea823bb921bc808c9a6279621ea9deb
mm548215 <mm548215@COB-CLAB59>
2019-11-17 17:47:25 -0500

    contextupdate

---
+++ DAL/MIS4200Team8Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MIS4200Team8v2.Models;
+using System.Data.Entity;
+
+namespace MIS4200Team8v2.DAL
+{
+    public class MIS4200Team8Context : DbContext
+    {
+        public MIS4200Team8Context() : base ("MIS4200Team8v11")
+        {
+
+        }
+        public DbSet<userDetail> userDetails{ get; set; }
+        public DbSet<sendPoints> sendPointss { get; set; }
+        public DbSet<coreValues> coreValuess { get; set; }
+
+
+    }
+}
