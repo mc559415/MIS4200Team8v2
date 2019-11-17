66ae6fc34d4da70f70199c650a7160a602593768
mc559415 <mc559415@ohio.edu>
2019-11-17 17:02:26 -0500

    sendPoints

---
+++ Models/sendPoints.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using MIS4200Team8v2.Models;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MIS4200Team8v2.Models
+{
+    public class sendPoints
+    {
+        [Key]
+        [Required]
+        public Guid ID { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+        [Required]
+        [Display(Name = "Core Value")]
+        public string coreValue { get; set; }
+        [Required]
+        [Display(Name = "Points")]
+        public int points { get; set; }
+        public Guid userID { get; set; }
+        [ForeignKey("userID")]
+        public virtual UserDetail UserDetail { get; set; }
+    }
+}
