bc73ed43daa2b5834881818ec1d4ddc743d3ee19
mc559415 <mc559415@ohio.edu>
2019-11-20 11:11:11 -0500

    Bullshit

---
+++ Migrations/MISContext/Configuration.cs
@@ -0,0 +1,25 @@
+namespace MIS4200Team8v2.Migrations.MISContext
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MIS4200Team8v2.DAL.MIS4200Team8Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            MigrationsDirectory = @"Migrations\MISContext";
+            ContextKey = "MIS4200Team8v2.DAL.MIS4200Team8Context";
+        }
+
+        protected override void Seed(MIS4200Team8v2.DAL.MIS4200Team8Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
