ba4b64b5bb9a599cacb6d122eb8356bfe03748c8
Maxwell Chappel <mc559415@ohio.edu>
2019-12-08 17:44:57 -0500

    es

--- Controllers/coreValuesController.cs
+++ Controllers/coreValuesController.cs
@@ -47,7 +47,7 @@ namespace MIS4200Team8v2.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "valueID,valueName,recognitionTime,description")] coreValues coreValues)
+        public ActionResult Create([Bind(Include = "valueID,valueName,description")] coreValues coreValues)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace MIS4200Team8v2.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "valueID,valueName,recognitionTime,description")] coreValues coreValues)
+        public ActionResult Edit([Bind(Include = "valueID,valueName,description")] coreValues coreValues)
         {
             if (ModelState.IsValid)
             {
